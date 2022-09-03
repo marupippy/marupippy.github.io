9c5f2e6528af9c1d09fb317f9f4e59bffef626aa
kitsune-apps <kitsune.apps@gmail.com>
2022-09-03 11:30:33 +0200

    Adding toggle menu

---
+++ js/pip-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(document).on("click",".js-menu-toggle",function(){if($("body").hasClass("menu--open")){$("body").removeClass("menu--open")}else{$("body").addClass("menu--open")}});$(document).on("click",".menu__list__item__link",function(){if($(".menu").hasClass("menu--open")){$(".menu").removeClass("menu--open")}})});
---
+++ js/pip.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    $(document).on('click', '.js-menu-toggle', function (){
+
+		// If already open
+		if ( $('body').hasClass('menu--open') ) {
+			$('body').removeClass('menu--open');
+		}
+
+		// If not open
+		else {
+			$('body').addClass('menu--open');
+		}
+	});
+
+	$(document).on('click', '.menu__list__item__link', function (){
+
+		// If menu is open when you click a link on mobile
+		if ( $('.menu').hasClass('menu--open') ) {
+			$('.menu').removeClass('menu--open');
+		}
+	});
+  
+});
