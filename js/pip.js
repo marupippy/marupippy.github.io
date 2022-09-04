860c5157cc39a8b31444f795e568771ac066c61e
kitsune-apps <kitsune.apps@gmail.com>
2022-09-04 14:06:22 +0200

    Adding active link styling

--- js/pip.js
+++ js/pip.js
@@ -1,20 +1,23 @@
 $(document).ready(function(){
 
-    $(document).on('click', '.js-menu-toggle', function (){
+	// The current page route (e.g. about/)
+	var navTarget = $('body').attr('data-page-url');
+	navTarget = navTarget.replace(/\/+$/, "");
 
-		// If already open
+	// Switch active link states
+	$('.active-link').removeClass('active-link');
+	$('a[href="' + navTarget + '"]').addClass('active-link');
+
+	// Toggle header menu
+    $(document).on('click', '.js-menu-toggle', function (){
 		if ( $('body').hasClass('menu--open') ) {
 			$('body').removeClass('menu--open');
-		}
-
-		// If not open
-		else {
+		} else {
 			$('body').addClass('menu--open');
 		}
 	});
 
 	$(document).on('click', '.menu__list__item__link', function (){
-
 		// If menu is open when you click a link on mobile
 		if ( $('.menu').hasClass('menu--open') ) {
 			$('.menu').removeClass('menu--open');
